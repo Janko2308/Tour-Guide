6c82beb36653c9026f05918ae01469131d10ea13
your name <your mail@technikum-wien.at>
2023-03-14 16:44:31 +0100

    Changes class 7

--- Tour_Planner/App.xaml.cs
+++ Tour_Planner/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Tour_Planner.ViewModels;
 
 namespace Tour_Planner
 {
@@ -13,5 +14,17 @@ namespace Tour_Planner
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // create all viewmodels (and inject them later)
+            var mainViewModel = new MainViewModel();
+
+
+            var wnd = new MainWindow
+            {
+                DataContext = mainViewModel
+            };
+            wnd.Show();
+        }
     }
 }
---
+++ Tour_Planner/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Tour_Planner.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string searchText = "banana";
+
+        public string SearchText
+        {
+            get => searchText;
+            set
+            {
+                searchText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SearchText)));
+            }
+        }
+
+        public MainViewModel()
+        {
+            SearchCommand = new RelayCommand(param => Debug.Print(searchText));
+        }
+
+        public ICommand SearchCommand
+        {
+            get;
+            
+        }
+
+    }
+}
