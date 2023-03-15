f9de632c3c8ab694e269108c2cf01e614b1fa55c
your name <your mail@technikum-wien.at>
2023-03-15 11:27:24 +0100

    Added BaseViewModel

---
+++ Tour_Planner/ViewModels/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tour_Planner.ViewModels
+{
+    class BaseViewModel : INotifyPropertyChanged
+
+
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        //Callermembername sets the propertyname automatically to the membername of the caller
+
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+        
+    }
+}
