3d9d8523503d66fda824901fd9b7277aedb62954
TORz69 <91666614+TORz69@users.noreply.github.com>
2023-05-08 23:26:36 +0200

    Implementation of the MapCreator

--- Tour_Planner.BL/TourManager.cs
+++ Tour_Planner.BL/TourManager.cs
@@ -4,12 +4,14 @@ using Tour_Planner.Model;
 namespace Tour_Planner.BL {
     public class TourManager {
         private IDataManager DataManager;
+        private MapCreator MapCreator;
 
         public TourManager(IDataManager dataManager) {
             this.DataManager = dataManager;
         }
 
         public void AddTour(TourItem t) {
+            t.TourInfo = MapCreator.CreateMap(t.From, t.To).Result;
             DataManager.AddTour(t);
         }
 
