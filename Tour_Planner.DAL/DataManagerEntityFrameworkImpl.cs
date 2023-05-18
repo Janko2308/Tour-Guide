165a5cb5efa1dffb3cc05980d13ae07a0652641d
TORz69 <91666614+TORz69@users.noreply.github.com>
2023-05-18 21:31:10 +0200

    Edit works

--- Tour_Planner.DAL/DataManagerEntityFrameworkImpl.cs
+++ Tour_Planner.DAL/DataManagerEntityFrameworkImpl.cs
@@ -21,8 +21,22 @@ namespace Tour_Planner.DAL {
         }
 
         public void EditTour(TourItem t) {
-            // TODO: change the values of the entity in DB with the t.Id Id value, to the values contained in t
+            // find item in DB with the same Id as t.Id
+            TourItem tour = context.TourItems.Find(t.Id);
+            if (tour == null) {
+                throw new Exception("Tour not found");
+            }
             
+            // change the values of the entity in DB with the t.Id Id value, to the values contained in t
+            tour.Name = t.Name;
+            tour.Description = t.Description;
+            tour.From = t.From;
+            tour.To = t.To;
+            tour.TourInfo = t.TourInfo;
+            tour.Distance = t.Distance;
+            tour.EstimatedTime = t.EstimatedTime;
+
+            context.SaveChanges();
         }
 
         public bool DeleteTour(TourItem t) {
