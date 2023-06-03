b9b9610a332d2e97cf4f3884c6f7fe0307fb8323
TORz69 <91666614+TORz69@users.noreply.github.com>
2023-06-03 11:31:04 +0200

    AddTourTest fails

--- Tour_Planner.Test/DatabaseMockTests.cs
+++ Tour_Planner.Test/DatabaseMockTests.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore.Storage;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Tour_Planner.DAL;
 using Tour_Planner.Model;
 
 namespace Tour_Planner.Test {
@@ -13,17 +16,38 @@ namespace Tour_Planner.Test {
         public void Setup() {
         }
 
-        // Create a mock repository and try adding a tour item
-        /*[Test]
-        public void AddTourItemTest() {
+        [Test]
+        public void AddTourTest() {
             // Arrange
-            TODO
+            var dal = new DataManagerEntityFrameworkImpl();
+            FieldInfo context = typeof(DataManagerEntityFrameworkImpl).GetField
+                ("context", BindingFlags.NonPublic | BindingFlags.Instance);
+            MethodInfo onConfiguring = typeof(TourDbContext).GetMethod
+                ("onConfiguring", BindingFlags.NonPublic | BindingFlags.Instance);
+            var mockContext = new Mock<TourDbContext>();
+            context.SetValue(dal, mockContext);
+            onConfiguring.Invoke(mockContext, new object[] { new DbContextOptionsBuilder() });
+            var t = new TourItem();
+            List<TourItem> tours = new List<TourItem>();
+            mockContext.Setup(x => x.TourItems.Add(It.IsAny<TourItem>())).Callback<TourItem>(tours.Add);
 
             // Act
-            bool result = database.AddTourItem(tour);
+            dal.AddTour(t);
 
             // Assert
-            Assert.IsTrue(result);
+            Assert.That(tours.Count, Is.EqualTo(1));
+            Assert.That(tours.Contains(t));
+            mockContext.Verify(x => x.SaveChanges(), Times.Once);
+        }
+
+        /*public void AddTour(TourItem t) {
+            // check if the t.name already exists
+            if (context.TourItems.Any(x => x.Name == t.Name)) {
+                throw new DbUpdateException($"Tour name {t.Name} already used");
+            }
+            
+            context.TourItems.Add(t);
+            context.SaveChanges();
         }*/
     }
 }
