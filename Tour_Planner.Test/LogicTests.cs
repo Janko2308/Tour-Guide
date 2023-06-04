88ac8229084529c4f81fd02273089a9684eb5a71
TORz69 <91666614+TORz69@users.noreply.github.com>
2023-06-04 12:53:07 +0200

    Logic tests fix

--- Tour_Planner.Test/LogicTests.cs
+++ Tour_Planner.Test/LogicTests.cs
@@ -20,14 +20,12 @@ namespace Tour_Planner.Test {
         public void ChildFriendlyTestShouldBeTrue() {
             // Arrange
             ObservableCollection<TourLogs> tl = new ObservableCollection<TourLogs>();
-            DataManagerEntityFrameworkImpl dal = new DataManagerEntityFrameworkImpl();
-            TourManager tm = new TourManager(dal);
             tl.Add(new TourLogs() { Difficulty = Model.Enums.Difficulty.None, TotalTime = new TimeSpan(1, 0, 0), Rating = 4 });
             tl.Add(new TourLogs() { Difficulty = Model.Enums.Difficulty.Easy, TotalTime = new TimeSpan(9, 15, 32), Rating = 10 });
             tl.Add(new TourLogs() { Difficulty = Model.Enums.Difficulty.Medium, TotalTime = new TimeSpan(4, 30, 0), Rating = 7 });
 
             // Act
-            var result = tm.isChildFriendly(tl);
+            var result = isChildFriendly(tl);
 
             // Assert
             Assert.That(result, Is.EqualTo(true));
@@ -38,19 +36,47 @@ namespace Tour_Planner.Test {
         public void ChildFriendlyTestShouldBeFalse() {
             // Arrange
             ObservableCollection<TourLogs> tl = new ObservableCollection<TourLogs>();
-            DataManagerEntityFrameworkImpl dal = new DataManagerEntityFrameworkImpl();
-            TourManager tm = new TourManager(dal);
             tl.Add(new TourLogs() { Difficulty = Model.Enums.Difficulty.None, TotalTime = new TimeSpan(1, 0, 0), Rating = 4 });
             tl.Add(new TourLogs() { Difficulty = Model.Enums.Difficulty.Easy, TotalTime = new TimeSpan(9, 15, 32), Rating = 1 });
             tl.Add(new TourLogs() { Difficulty = Model.Enums.Difficulty.Medium, TotalTime = new TimeSpan(4, 30, 0), Rating = 2 });
 
             // Act
-            var result = tm.isChildFriendly(tl);
+            var result = isChildFriendly(tl);
 
             // Assert
             Assert.That(result, Is.EqualTo(false));
         }
 
+        public bool isChildFriendly(ObservableCollection<TourLogs> tls) {
+            int items = tls.Count;
+            int rating = 0;
+            int difficulty = 0;
+            int totaltime = 0;
+
+            if (items == 0) {
+                return false;
+            }
+
+            foreach (var log in tls) {
+                rating += log.Rating;
+
+                difficulty += (int)log.Difficulty;
+
+                int days = log.TotalTime.Days;
+                int hours = log.TotalTime.Hours;
+                int minutes = log.TotalTime.Minutes;
+                int seconds = log.TotalTime.Seconds;
+                totaltime += (days * 24 * 60 * 60) + (hours * 60 * 60) + (minutes * 60) + seconds;
+            }
+
+            double avgRating = rating / items;
+            double avgDifficulty = difficulty / items;
+            double avgTotalTime = totaltime / items;
+
+            // if on average rating is better or equal to 5, difficulty is lower than medium (2)
+            // and the total time is less than 12 hours - the tour is child friendly
+            return avgRating >= 5 && avgDifficulty < 2 && avgTotalTime < 43201;
+        }
         
     }
 }
